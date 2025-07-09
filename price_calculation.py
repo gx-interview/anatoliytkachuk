da53e8d98e99ed437a0ab9a3e6a603ff5a7eac58
anatoliy-tkachuk <priler92@gmail.com>
2025-07-09 18:01:45 +0200

    feat: Add PriceCalculator for data validation & "Total" calculation
    - Parse and convert timestamps to UTC+6
    
    - Validate presence, types, and non-null of required columns
    
    - Detect duplicate timestamps for Product A
    
    - Calculate “Total” for Product A & B with purity discounts

--- price_calculation.py
+++ price_calculation.py
@@ -1,7 +1,10 @@
 import enum
 import os
 
+import numpy as np
 import pandas as pd
+
+from pandas.core.dtypes.common import is_string_dtype, is_numeric_dtype
 
 
 
@@ -69,3 +72,81 @@ class DataHandler:
         raise NotImplementedError('API post is not implemented')
 
 
+class PriceCalculator:
+    """
+    Calculate total prices in validated dataframe.
+    Public method calculate() includes private methods with steps of serialization, validation and calculation.
+    """
+
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.df: pd.DataFrame = df
+
+    def calculate(self) -> pd.DataFrame:
+        self._serialize_data()
+        self._validate_data()
+        self._update_timezone()
+        self._calculate_total()
+        return self.df
+
+    def _serialize_data(self) -> None:
+        self.df['Datetime'] = pd.to_datetime(self.df['Datetime'])
+
+    def _validate_data(self) -> None:
+        # Validation for missing columns required for the task in the dataframe
+        list_of_required_columns = ['Name', 'Datetime', 'Amount', 'Price', 'Purity']
+        missing = [col for col in list_of_required_columns if col not in self.df.columns]
+        if missing:
+            raise ValueError(f"Missing required columns: {', '.join(missing)}. ")
+
+        if self.df.isnull().values.any():
+            raise ValueError('The dataframe contains null values.')
+
+        if not is_numeric_dtype(self.df['Amount']) or not is_numeric_dtype(self.df['Price']):
+            raise ValueError('Amount and price are not numeric.')
+        if not is_string_dtype(self.df['Name']):
+            raise ValueError('Name column is not a string.')
+
+        # From the 'Step 3' of the task it is logical that a product should have a single price at a given timestamp
+        duplicates_count = self.df[self.df['Name'] == 'ProductA'].duplicated(subset=['Datetime']).sum()
+        if duplicates_count > 0:
+             raise ValueError('Product A has duplicate timestamps')
+
+    def _update_timezone(self) -> None:
+        """
+        Convert values in datetime column from timezone UTC to UTC+6
+        """
+        if self.df['Datetime'].dt.tz is not None:
+            self.df['Datetime'] = self.df['Datetime'].dt.tz_convert('UTC').dt.tz_convert('Etc/GMT-6')
+        else:
+            self.df['Datetime'] = self.df['Datetime'].dt.tz_localize('UTC').dt.tz_convert('Etc/GMT-6')
+
+    def _calculate_total(self) -> None:
+        """
+        Add new column - total - where:
+        Product A is an outright and, if product purity is 'Pure', the total should therefore be calculated as amount * price
+        Product B is a diff and the total should be found by taking its respective Product A price (the one with matching timestamp) from the price of Product B given, before multiplying by the amount of Product B
+        If the Product purity is 'Impure' prices should be reduced to 3/4 of its given value when calculating the total
+
+        Implementation via np.select (more efficient)
+        Alternative implementation: LEFT JOIN on datetime and calculating difference between price columns
+        """
+        conditions: list = [
+            (self.df['Name'] == 'ProductA') & (self.df['Purity'] == 'Pure'),
+            (self.df['Name'] == 'ProductA') & (self.df['Purity'] == 'Impure'),
+            (self.df['Name'] == 'ProductB') & (self.df['Purity'] == 'Pure'),
+            (self.df['Name'] == 'ProductB') & (self.df['Purity'] == 'Impure'),
+        ]
+
+        # Creating series of Product A with Datetime as index
+        product_a_series: pd.Series = self.df[self.df['Name'] == 'ProductA'].set_index('Datetime')['Price']
+        # Maps prices of Product B and Product A based on the same timestamp and calculates the difference
+        price_difference: pd.Series = self.df['Price'] - self.df['Datetime'].map(product_a_series)
+        results: list = [
+            self.df['Price'] * self.df['Amount'],
+            self.df['Price'] * 0.75 * self.df['Amount'],
+            price_difference * self.df['Amount'],
+            price_difference * 0.75 * self.df['Amount'],
+        ]
+        self.df['Total'] = np.select(
+            conditions, results, default=np.nan
+        )
